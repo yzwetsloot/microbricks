ab51bbc0d0dff842e25b115c1b5cb206f841a4a8
Youri Zwetsloot <yourizwetsloot@yahoo.com>
2021-10-12 16:07:23 +0200

    Add statistics fetch call

--- client/src/components/StatBox.js
+++ client/src/components/StatBox.js
@@ -1,21 +1,27 @@
+import Loader from "react-loader-spinner";
+
 function StatBox(props) {
   const formatter = new Intl.NumberFormat("en-US");
 
-  return (
-    <div className="StatBox">
-      <span className="statTitle">{props.name}</span>
-      <span className="statMain">{formatter.format(props.main)}</span>
-      <span style={{ color: props.change > 0 ? "#24c010" : "#c01025" }}>
-        {props.change > 0 ? "+" : ""}
-        {formatter.format(props.change)}&nbsp;
-        {props.change > 0 ? (
-          <i class="fas fa-angle-up"></i>
-        ) : (
-          <i class="fas fa-angle-down"></i>
-        )}
-      </span>
-    </div>
-  );
+  if (props.loading) {
+    return <Loader type="ThreeDots" color="#9c88ff" />;
+  } else {
+    return (
+      <div className="StatBox">
+        <span className="statTitle">{props.name}</span>
+        <span className="statMain">{formatter.format(props.main)}</span>
+        <span style={{ color: props.change > 0 ? "#24c010" : "#c01025" }}>
+          {props.change > 0 ? "+" : ""}
+          {formatter.format(props.change)}&nbsp;
+          {props.change > 0 ? (
+            <i class="fas fa-angle-up"></i>
+          ) : (
+            <i class="fas fa-angle-down"></i>
+          )}
+        </span>
+      </div>
+    );
+  }
 }
 
 export default StatBox;
--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -1,16 +1,40 @@
+import { useState, useEffect } from "react";
+
 import StatBox from "./StatBox";
 
 function Statistics(props) {
-  const stats = [
-    { name: "1 day products", main: 57319, change: -2381 },
-    { name: "Total products", main: 156098, change: 3298 },
-    { name: "Average price", main: 20.34, change: 1.02 },
-  ];
+  const [statistics, setStatistics] = useState([
+    { name: "1 day products", main: 0, change: 0 },
+    { name: "Total products", main: 0, change: 0 },
+    { name: "Average price", main: 0, change: 0 },
+  ]);
+
+  const [loading, setLoading] = useState(false);
+
+  const getStatistics = () => {
+    setLoading(true);
+
+    fetch("/p/getStatistics")
+      .then((response) => response.json())
+      .then((body) => {
+        setStatistics(body);
+        console.info(`Received ${body.length} statistics`);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(`Failed to retrieve statistics: ${err.stack}`);
+        setLoading(false);
+      });
+  };
+
+  // componentDidMount
+  useEffect(getStatistics, []);
 
   return (
     <div style={{ display: "flex" }} className="Statistics">
-      {stats.map((statistic, i) => (
+      {statistics.map((statistic, i) => (
         <StatBox
+          loading={loading}
           name={statistic.name}
           main={statistic.main}
           change={statistic.change}
